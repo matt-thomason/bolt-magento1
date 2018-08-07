85e8dfeb665b5f3ee330e51623d8bd7a686d97ea
Monish SP <monishsp@gmail.com>
2018-08-07 16:22:36 -0600

    WIP attributes code
    The attributes value say "No" instead of the actual value. In the demo magento store the front end has the color/size defined but it looks like the backend doesn't.

--- app/code/community/Bolt/Boltpay/Helper/Api.php
+++ app/code/community/Bolt/Boltpay/Helper/Api.php
@@ -662,6 +662,15 @@ class Bolt_Boltpay_Helper_Api extends Bolt_Boltpay_Helper_Data
                     $imageUrl = $boltHelper->getItemImageUrl($item);
                     $product   = Mage::getModel('catalog/product')->load($item->getProductId());
                     $type = $product->getTypeId() == 'virtual' ? self::ITEM_TYPE_DIGITAL : self::ITEM_TYPE_PHYSICAL;
+                    
+                    // WIP Debug code for capturing attributes of product to add to the cart array below    
+                    $attribute = $product->getResource()->getAttribute('color');
+                    echo "<script>console.log('Color')</script>";
+                    echo "<script>console.log(".json_encode($attribute->getFrontend()->getValue($item->getProduct())).")</script>";
+                    
+                    $attribute = $product->getResource()->getAttribute('size');
+                    echo "<script>console.log('Size')</script>";
+                    echo "<script>console.log(".json_encode($attribute->getFrontend()->getValue($item->getProduct())).")</script>";
 
                     $calculatedTotal += round($item->getPrice() * 100 * $item->getQty());
                     return array(
