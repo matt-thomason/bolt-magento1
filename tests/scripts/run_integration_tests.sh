127e2d661e617233818d3365b4ac130043f0aa36
daisy1754 <d-part@hotmail.co.jp>
2018-12-01 21:20:21 -0800

    Install node 8.9.4 before running test

--- tests/scripts/run_integration_tests.sh
+++ tests/scripts/run_integration_tests.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+echo "Installing node"
+rm -rf ~/.nvm
+git clone https://github.com/creationix/nvm.git ~/.nvm
+cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
+source ~/.nvm/nvm.sh
+nvm install 8.9.4
+
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
+echo "Downloading integration test repo"
 cd /home/travis/build/BoltApp/bolt-magento1/
-
 git clone https://${INTEGRATION_TESTS_TOKEN}@github.com/BoltApp/integration-tests.git/
 
+echo "Installing depedencies"
 cd integration-tests
-
 npm install
 
+echo "Start test"
 TEST_ENV=sandbox xvfb-run npm run test-checkout-magento1
